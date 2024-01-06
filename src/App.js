1446120cb922d1e1e03e656b473b30c12161992d
EasyJake <easyj4ke@gmail.com>
2024-01-06 11:19:14 -0800

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,33 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
+
+<Button variant="danger">Danger</Button>{' '}
+
+import Nav from 'react-bootstrap/Nav'
+
+
+import Button from 'react-bootstrap/Button';
+
+function TypesExample() {
+  return (
+    <>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button>{' '}
+      <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button>{' '}
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+    </>
+  );
+}
+
+
+
+export default TypesExample;
+
 
 
 
